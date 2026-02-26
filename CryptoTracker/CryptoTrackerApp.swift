bf2d7a16a3d129f92245b71fd1972e18e170896f
Cesario Nivar <ing.cesarionivar@gmail.com>
2026-02-26 18:42:24 -0500

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Cesario Nivar on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
