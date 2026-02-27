c099c8568608b0ded1f8a1d7d0769407cfdd8a80
Cesario Nivar <ing.cesarionivar@gmail.com>
2026-02-26 20:16:15 -0500

    Create Coin Model

--- CryptoTracker/Models/CoinModel.swift
+++ CryptoTracker/Models/CoinModel.swift
@@ -5,4 +5,80 @@
 //  Created by Cesario Nivar on 2/26/26.
 //
 
-import Foundation
+
+// CoinGecko API info
+//
+//
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+//
+//  Base endpoint = https://api.coingecko.com/api/v3/
+//
+// Gecko Crypto API_KEY =  CG-PZ4nQRFNBQKnLTyFZBT1ucXj
+//
+//
+//
+
+struct CoinModel: Identifiable, Codable {
+    let id, symbol, name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap: Double?
+    let marketCapRank: Double?
+    let fullyDilutedValuation: Double?
+    let totalVolume, high24H, low24H, priceChange24H: Double?
+    let priceChangePercentage24H, marketCapChange24H, marketCapChangePercentage24H, circulatingSupply: Double?
+    let totalSupply: Double?
+    let maxSupply: Double?
+    let ath, athChangePercentage: Double?
+    let athDate: String?
+    let atl, atlChangePercentage: Double?
+    let atlDate: String?
+    let lastUpdated: String?
+    let priceChangePercentage1HInCurrency: Double?
+    let currentHoldings: Double?
+
+    enum CodingKeys: String, CodingKey {
+        case id, symbol, name, image
+        case currentPrice = "current_price"
+        case marketCap = "market_cap"
+        case marketCapRank = "market_cap_rank"
+        case fullyDilutedValuation = "fully_diluted_valuation"
+        case totalVolume = "total_volume"
+        case high24H = "high_24h"
+        case low24H = "low_24h"
+        case priceChange24H = "price_change_24h"
+        case priceChangePercentage24H = "price_change_percentage_24h"
+        case marketCapChange24H = "market_cap_change_24h"
+        case marketCapChangePercentage24H = "market_cap_change_percentage_24h"
+        case circulatingSupply = "circulating_supply"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
+        case ath
+        case athChangePercentage = "ath_change_percentage"
+        case athDate = "ath_date"
+        case atl
+        case atlChangePercentage = "atl_change_percentage"
+        case atlDate = "atl_date"
+        case lastUpdated = "last_updated"
+        case priceChangePercentage1HInCurrency = "price_change_percentage_1h_in_currency"
+        case currentHoldings
+    }
+    
+    func updateHoldings(amount: Double) -> CoinModel {
+        return CoinModel(id: id, symbol: symbol, name: name, image: image, currentPrice: currentPrice, marketCap: marketCap, marketCapRank: marketCapRank, fullyDilutedValuation: fullyDilutedValuation, totalVolume: totalVolume, high24H: high24H, low24H: low24H, priceChange24H: priceChange24H, priceChangePercentage24H: priceChangePercentage24H, marketCapChange24H: marketCapChange24H, marketCapChangePercentage24H: marketCapChangePercentage24H, circulatingSupply: circulatingSupply, totalSupply: totalSupply, maxSupply: maxSupply, ath: ath, athChangePercentage: athChangePercentage, athDate: athDate, atl: atl, atlChangePercentage: atlChangePercentage, atlDate: atlDate, lastUpdated: lastUpdated, priceChangePercentage1HInCurrency: priceChangePercentage1HInCurrency, currentHoldings: amount)
+    }
+    
+    var currentHoldingsValue: Double {
+        return (currentHoldings ?? 0) * currentPrice
+    }
+    
+    var rank: Int {
+        return Int(marketCapRank ?? 0)
+    }
+}
+
+enum Currency: String, Codable {
+    case btc = "btc"
+    case eth = "eth"
+    case usd = "usd"
+}
