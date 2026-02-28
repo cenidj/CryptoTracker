2462fdb1c50b0fcfcc0d4bdc7853d73f69b7f3b5
Cesario Nivar <ing.cesarionivar@gmail.com>
2026-02-28 00:02:32 -0500

    Saving images in the File Manager

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -14,19 +14,33 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         self.getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("[✨] Retrived image from File Manager")
+        } else {
+            downloadCoinImage()
+            print("[⤵️] Downloading image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else {
             return
         }
         
         imageSubscription = NetworkingManager.download(url: url)
-            .tryMap({ data -> UIImage? in
+            .tryMap({ (data) -> UIImage? in
                 return UIImage(data: data)
             })
             .sink(
@@ -34,8 +48,10 @@ class CoinImageService {
                     NetworkingManager.handleCompletion(completion: completion)
                 },
                 receiveValue: { [weak self] (returnedImage) in
-                    self?.image = returnedImage
-                    self?.imageSubscription?.cancel()
+                    guard let self = self, let downloadedImage = returnedImage else { return }
+                    self.image = downloadedImage
+                    self.imageSubscription?.cancel()
+                    self.fileManager.saveImage(image: downloadedImage, imageName: imageName, folderName: folderName)
                 }
             )
     }
